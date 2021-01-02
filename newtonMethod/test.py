8009ec292b61bbd62959dee963932772d0cec372
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-02 21:36:52 +0100

    feat: Add test cases

--- newtonMethod/test.py
+++ newtonMethod/test.py
@@ -1,10 +1,25 @@
-from python_code import Newton
+import unittest
 
-newton = Newton()
+from newton import Newton
 
-print("Derivative is {}".format(newton.calculate_derivative(500)))
-print("Function value: {}".format(abs(newton.calculate_f_value(500))))
 
-sol, num_iterations = newton.calculate_roots(500)
-print(sol, num_iterations)
+class TestGaussianClass(unittest.TestCase):
+    def setUp(self):
+        self.newton = Newton(f='x**2 - 9', max_iter=1e6)
+
+    def test_initialization(self): 
+        self.assertEqual(self.newton.f, 'x**2 - 9', 'incorrect function')
+        self.assertEqual(self.newton.max_iter, 1e6, 'incorrect maximum number of iterations')
+    
+    def test_solution1calc(self):
+        self.assertEqual(round(self.newton.find_solution(1000), 2), 3.00, 'root incorrect')
+
+    def test_solution2calc(self):
+        self.assertEqual(round(self.newton.find_solution(-1000), 2), -3.00, 'root incorrect')
+
+    def test_handlingZeroDivision(self):
+        self.assertEqual(round(self.newton.find_solution(0), 2), 3.00, 'Zero Division error occured')
+
+if __name__ == '__main__':
+    unittest.main()
 
