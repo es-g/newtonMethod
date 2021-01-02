26fc0bf8ddf2246ce1622e36544f9151864b2e6a
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-02 22:44:11 +0100

    style: Add docstring descriptions to functions

--- newtonMethod/newton.py
+++ newtonMethod/newton.py
@@ -2,33 +2,65 @@ import sympy as sym
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Newton():
+
     def __init__(self, f='x**2', max_iter=1e6, eps=1e-14):
-        self.f = f
+        """ Newton method class to find the solution
+
+        Attributes:
+                f (str) representing the function
+                max_iter (int) representing the maximum number of iterations to find the solution
+                eps (float) representing stopping criteria abs(f(x)) < epsilon
+                """
+        self.f = f  # Symbolic expression of the function
         self.max_iter = max_iter
         self.eps = eps
-
-        # Define x as mathematical symbol
-        x = sym.symbols('x')
+        x = sym.symbols('x')  # Define x as mathematical symbol
         self.x = x
 
     def calculate_f_value(self, x):
+        """ Function to evaluate function for given x
+
+            Args:
+                x (float): x value
+
+            Returns:
+                f(x) (float): value of the function for the given x
+
+            """
+
+        # lambdify provides a bridge from Sympy expression to numerical libraries
         f = sym.lambdify([self.x], self.f)
         return f(x)
- 
-        
+
     def calculate_derivative(self, x):
-        # Symbolic expression of the function - user input
-        
+        """ Function to find the derivative and calculate the dfdx value for given x
+
+            Args:
+                x (float): x value
+
+            Returns:
+                dfdx(x) (float): derivate of f(x) at given x
+            """
+
         self.dfdx_expr = sym.diff(self.f, self.x)
         dfdx = sym.lambdify([self.x], self.dfdx_expr)
-        
+
         return dfdx(x)
-    
+
     def plot_function(self, a=-10, b=10):
-        x = np.linspace(a,b,100)
+        """ Function to plot the given function
+
+            Args:
+                a, b (float) [optional]: intervals
+
+            Returns:
+                None
+            """
+        x = np.linspace(a, b, 100)
         y = sym.lambdify([self.x], self.f)(x)
-        
+
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         ax.spines['left'].set_position('center')
@@ -39,29 +71,38 @@ class Newton():
         ax.yaxis.set_ticks_position('left')
         plt.plot(x, y)
         plt.show()
-        
 
-    def find_solution(self, x0):
+    def find_solution(self, xn):
+        """ Function to approximate solution of f(x)=0 by Newton's method
+
+            Args:
+                x0 (float): initial guess for a solution f(x)=0
+
+            Returns:
+                xn (float): intercept (solution) by the formula x = xn - f(xn)/dfdx(xn)
+
+        """
+        xn = x0
         iter_counter = 0
-        f_value = self.calculate_f_value(x0)
+        f_value = self.calculate_f_value(xn)
         while abs(f_value) > self.eps and iter_counter < self.max_iter:
             try:
-                x0 = x0 - float(f_value) / self.calculate_derivative(x0)
+                xn = xn - float(f_value) / self.calculate_derivative(xn)
             except ZeroDivisionError:
-                # Handling ZeroDivisonError - if the derivative of the initial guess is 0, increment x0 
-                print("Error! - derivative zero for x = {}. Incrementing by 1...".format(x0))
-                x0 += 1
-            f_value = self.calculate_f_value(x0)
+                # Handling ZeroDivisonError - if the derivative of the initial guess is 0, increment x0
+                print(
+                    "Error! - derivative zero for x = {}. Incrementing by 1...".format(xn))
+                xn += 1
+            f_value = self.calculate_f_value(xn)
             iter_counter += 1
-            
-            print("X = {}, f(x) = {}, iteration #{}".format(x0, f_value, iter_counter))
 
-        print("Solution found at {} with {} iterations".format(x0, iter_counter))
+            print("x = {}, f(x) = {}, iteration #{}".format(
+                xn, f_value, iter_counter))
+
+        print("Solution found at {} with {} iterations".format(xn, iter_counter))
         if abs(f_value) > self.eps:
             print("Solution not found! Try changing the initial guess")
             iter_counter = -1
-            x0 = None
-
-        return x0
+            xn = None
         
-        
+        return xn
