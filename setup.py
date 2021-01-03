79954f3ca03872ba2d410670c84da6a53875e4e7
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-03 13:50:51 +0100

    feat: create setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-# TODO: Fill out this file with information about your package
+import setuptools
 
-# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-# HINT: Here is an example of a setup.py file
-# https://packaging.python.org/tutorials/packaging-projects/
+setuptools.setup(
+    name="newtonMethod",
+    version="0.1",
+    author="Yesbol Gabdullin",
+    author_email="yesbol_gabdullin@hotmail.com",
+    description="Package to find a good approximation for the root of a real-valued function f(x)=0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/es-g/newtonMethod",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
