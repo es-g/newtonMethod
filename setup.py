4843cf9170154a36e5fea028a5b3249a3909f5d0
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-03 14:47:03 +0100

    refactor: Remove long description

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="newtonMethod",
     version="0.1",
     author="Yesbol Gabdullin",
     author_email="yesbol_gabdullin@hotmail.com",
     description="Package to find a good approximation for the root of a real-valued function f(x)=0",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/es-g/newtonMethod",
     packages=setuptools.find_packages(),
     classifiers=[
